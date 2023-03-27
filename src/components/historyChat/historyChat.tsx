22907567bdcbcf54f51f4cf394abd5b97fa8da7a
zhuaoshuai13 <531024850@qq.com>
2023-03-27 18:02:44 +0800

    feat: ui

--- src/components/historyChat/historyChat.tsx
+++ src/components/historyChat/historyChat.tsx
@@ -5,7 +5,7 @@ interface HistoryChatPropsType {
 const HistoryChat = (props: HistoryChatPropsType) => {
   const { info } = props;
   return (
-    <div className=" mt-[10px] w-full cursor-pointer overflow-clip truncate text-ellipsis rounded-[3px] border-2 border-solid border-gray-50  p-[8px] text-white transition hover:bg-[#444655]">
+    <div className=" mt-[10px] w-full cursor-pointer overflow-clip truncate text-ellipsis rounded-[3px] border border-solid border-gray-300  p-[8px] text-white transition hover:bg-[#444655]">
       {info}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,32 @@ export default function Home() {
     <>
       <div className="flex h-screen w-screen">
         <div className="w-[260px] bg-black p-[5px]">
-          <HistoryChat info="为什么人类要吃饭为什么人类要吃饭为什么人类要吃饭为什么人类要吃饭" />
+          <HistoryChat info="test" />
           <HistoryChat info="啊啊啊" />
         </div>
-        <div className="flex-1 bg-[#444655]"></div>
+        <div className="flex flex-1 flex-col bg-[#444655] py-[20px]">
+          <div className="h-[50px] text-center text-[30px] leading-[50px] text-white">
+            ChatGpt
+          </div>
+          <div className="flex-1">
+            {/* 没有内容 */}
+            <div className="relative h-full w-full">
+              <div className=" absolute left-0 right-0 top-0 bottom-0 m-auto h-fit w-fit text-[60px] text-white">
+                广告位招租
+              </div>
+            </div>
+          </div>
+          <div className="m-aut0 w-full">
+            <div className="m-auto flex w-4/5">
+              <input
+                type="textarea"
+                className="block flex-1 rounded-[5px] border-none bg-black text-white outline-none"
+                placeholder="请输入内容"
+              />
+              <div className="text-white">发送</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
