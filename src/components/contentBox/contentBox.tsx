9d8e4361ec3fce46239985f7594d315b48024b09
zhuaoshuai13 <531024850@qq.com>
2023-05-09 15:12:42 +0800

    feat: 发送消息

--- src/components/contentBox/contentBox.tsx
+++ src/components/contentBox/contentBox.tsx
@@ -2,15 +2,15 @@ import { RightChatBox, LeftChatBox, InputBox } from "@/components";
 import { useState } from "react";
 
 const ContentBox = () => {
-  const [msgList, setMsgList] = useState<[string]>(["first"]);
+  const [msgList, setMsgList] = useState<Array<string>>([]);
 
-  const handleChildValue = (value: any) => {
-    setMsgList(value);
+  const handleChildValue = (value: []) => {
+    setMsgList([...value]);
   };
   return (
     <div className="flex flex-1 flex-col bg-[#444655] py-[20px]">
       <div className="h-[50px] text-center text-[30px] leading-[50px] text-white">
-        ChatGpt
+        Chat
       </div>
       <div className="flex-1 overflow-scroll">
         <div className="relative m-auto h-full w-4/5">
--- src/components/inputBox/inputBox.tsx
+++ src/components/inputBox/inputBox.tsx
@@ -3,34 +3,43 @@ import ky from "ky";
 import Send from "@/src/asset/send.svg";
 import { useState } from "react";
 
-const Input = (props: any) => {
-  const [msgList, setMsgList] = useState<[string]>(["first"]);
+const Input = (props: { getValue: Function }) => {
+  const [msgList, setMsgList] = useState<Array<string>>([]);
 
   const [info, setInfo] = useState("");
 
   const handleInput = (e: any) => {
     const msg = e.target.value;
-
     setInfo(msg);
   };
 
   const click = () => {
-    const temp = msgList;
-    temp.push(info);
-    props.getValue(temp);
-    setMsgList(temp);
+    if (info === "") return;
+    const newMsgArray = [...msgList, info];
+    props.getValue(newMsgArray);
+    setMsgList(newMsgArray);
+    setInfo("");
+
+    // ky.post("http://localhost:9527/chat", {
+    //   json: {
+    //     msg: info,
+    //   },
+    //   timeout: 100000,
+    // });
   };
 
   return (
     <div className="m-aut0 w-full">
-      <div className="m-auto flex w-[90%] items-center">
+      <div className="m-auto flex w-[90%] items-center pt-2">
         <textarea
-          className="textarea textarea-md w-full text-white"
+          className="textarea textarea-md mr-3 w-full text-white"
           placeholder=""
           onInput={handleInput}
+          value={info}
         ></textarea>
-        <button className="btn-ghost btn" onClick={click}>
+        <button className="btn-ghost btn text-white" onClick={click}>
           <Send />
+          <span className="ml-1">发送</span>
         </button>
       </div>
     </div>
