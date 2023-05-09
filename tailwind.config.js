23008b2cc3e697a2c9aa2dd295154139c55cf975
zhuaoshuai13 <531024850@qq.com>
2023-05-09 15:12:14 +0800

    feat: 增加消息入场动画

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) 1",
+        textIn: "textIn 0.5s ease-in-out 1",
+      },
+      keyframes: {
+        textIn: {
+          "0%": { transform: "translateX(20px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
