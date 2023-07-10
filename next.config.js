71cbf17e1f882cf10dba3e75edd2185b33bc6332
zhuaoshuai13 <531024850@qq.com>
2023-07-10 17:00:36 +0800

    feat: 配置更改

--- next.config.js
+++ next.config.js
@@ -4,17 +4,17 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
+    contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  webpack: config => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     }); // 针对 SVG 的处理规则
     return config;
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
